63320e3786c6a98a39f27d606d4587f54966e6f9
DanielaReinhardt <99242852+DanielaReinhardt@users.noreply.github.com>
2024-02-06 13:55:41 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+provider "google" {
+  project = "de0360-sbx-data-academy-dr"  # Ihr Google Cloud Projekt-ID
+  region  = "europe-west3"                # Region, in der die VM erstellt werden soll
+}
 
+resource "google_compute_instance" "vm_instance" {
+  name         = var.instance_terraform2
+  machine_type = var.e2_micro
+  zone         = "europe-west3-c"
+  
+  boot_disk {
+    initialize_params {
+      image = "debian-12-bookworm-v20240110"   # Beispiel-Image, bitte anpassen
+    }
+  }
+
+  network_interface {
+    network = "sbx-spoke-0"  # Beispiel-Netzwerk, bitte anpassen
+    access_config {}
+  }
+}
