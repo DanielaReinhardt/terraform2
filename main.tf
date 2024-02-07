33e750360c49380925e26ac25ae63baf42f22032
DanielaReinhardt <99242852+DanielaReinhardt@users.noreply.github.com>
2024-02-07 09:18:22 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,16 +8,7 @@ variable location {}
 resource "google_compute_instance" "vm_instance" {
   project      = var.project
   name         = var.project 
-  location     =var.location
+  location     = var.location
   
-  boot_disk {
-    initialize_params {
-      image = "debian-12-bookworm-v20240110"
-    }
-  }
-
-  network_interface {
-    network = "projects/de0360-2-sbx-net-spoke-1/global/networks/sbx-spoke-0"
-    access_config {}
   }
 # [END google_storage_bucket] 
